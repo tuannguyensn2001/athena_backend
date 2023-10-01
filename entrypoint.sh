7e56d0731fe5a8df21aaf2b37c049d6e0859491a
Tuan Nguyen <tuannguyen@Tuans-MacBook-Pro.local>
2023-10-01 14:38:05 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,12 @@
 
 case "$1" in
   "rails")
+    echo $DATABASE_HOST
+    echo $DATABASE_PORT
+    echo $DATABASE_PASSWORD
+    echo $RAILS_ENV
     bundle exec rails db:migrate
-    bundle exec puma
+    bundle exec rails s
     ;;
   "sidekiq")
     bundle exec rails db:migrate
