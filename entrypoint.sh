8ef0457aa746a6edfb3d1c661f901d78564c17c7
tuannguyensn2001 <devpro2001@gmail.com>
2023-10-22 09:10:28 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 case "$1" in
-  "rails")
-    echo $DATABASE_HOST
-    echo $DATABASE_PORT
-    echo $DATABASE_PASSWORD
-    echo $RAILS_ENV
-    bundle exec rails db:migrate
-    /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
-#    bundle exec rails s
-    ;;
-  "test")
-      echo $DATABASE_HOST
-      echo $DATABASE_PORT
-      echo $DATABASE_PASSWORD
-      echo $RAILS_ENV
-      echo $DATABASE_USR
-      bundle exec rails db:setup
-      bundle exec rspec
-    ;;
-  "sidekiq")
-    bundle exec rails db:migrate
-#    bundle exec sidekiq
-    ;;
-  *)
-    exec "$@"
-    ;;
+"rails")
+  echo $DATABASE_HOST
+  echo $DATABASE_PORT
+  echo $DATABASE_PASSWORD
+  echo $RAILS_ENV
+  bundle exec rails db:migrate
+  #    /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+  bundle exec rails s
+  ;;
+"test")
+  echo $DATABASE_HOST
+  echo $DATABASE_PORT
+  echo $DATABASE_PASSWORD
+  echo $RAILS_ENV
+  echo $DATABASE_USR
+  bundle exec rails db:setup
+  bundle exec rspec
+  ;;
+"sidekiq")
+  bundle exec rails db:migrate
+  bundle exec sidekiq
+  ;;
+"metrics")
+  bundle exec prometheus_exporter
+  ;;
+*)
+  exec "$@"
+  ;;
 esac
