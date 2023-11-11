ddba00f53b6b69586eac646b2f697889853933fa
tuannguyensn2001 <devpro2001@gmail.com>
2023-11-11 14:45:26 +0700

    fix

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   require 'prometheus_exporter/middleware'
-  require './app/middleware/metric_middleware.rb'
+  # require './app/middleware/metric_middleware.rb'
 
   # This reports stats per request like HTTP status and timings
 
