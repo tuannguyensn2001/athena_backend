a1373a4f150449ebf244edbecbdbf3af6387b2a4
tuannguyensn2001 <devpro2001@gmail.com>
2023-11-11 15:07:27 +0700

    fix

--- config/initializers/telemetry.rb
+++ config/initializers/telemetry.rb
@@ -1,12 +1,14 @@
 require 'opentelemetry/sdk'
 require 'opentelemetry/instrumentation/all'
 require 'opentelemetry-exporter-otlp'
-OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'athena'
-  # c.use_all() # enables all in
-  c.use_all
-  # c.use 'OpenTelemetry::Instrumentation::ActiveRecord'
-  # c.use 'OpenTelemetry::Instrumentation::Http'
 
+if Rails.env.development?
+  OpenTelemetry::SDK.configure do |c|
+    c.service_name = 'athena'
+    # c.use_all() # enables all in
+    c.use_all
+    # c.use 'OpenTelemetry::Instrumentation::ActiveRecord'
+    # c.use 'OpenTelemetry::Instrumentation::Http'
 
+  end
 end
